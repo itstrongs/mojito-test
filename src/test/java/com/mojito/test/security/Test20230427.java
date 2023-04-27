f9c3e5f0ab6c12a817873a7a906b1e3ca0be12d1
liufq <accounts_630839f290b1cc63544e5c35@mail.teambition.com>
2023-04-27 16:38:05 +0800

    update

--- src/test/java/com/mojito/test/security/Test20230427.java
+++ src/test/java/com/mojito/test/security/Test20230427.java
@@ -1,5 +1,6 @@
 package com.mojito.test.security;
 
+import com.alibaba.excel.util.StringUtils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -19,13 +20,15 @@ public class Test20230427 {
 
     @Test
     public void test() throws IOException {
+        String json = FileUtils.getString("增加sla值-预发.json");
 //        String json = FileUtils.getString("增加sla值-MSS.json");
-        String json = FileUtils.getString("增加sla值-国信.json");
+//        String json = FileUtils.getString("增加sla值-国信.json");
         List<SlaUpdate> slaUpdates = JSON.parseArray(json, SlaUpdate.class);
         for (SlaUpdate slaUpdate : slaUpdates) {
             JSONArray jsonArray = JSON.parseArray(slaUpdate.getExtend_column());
 
             String sla = null;
+            String weight = null;
             for (int i = 0; i < jsonArray.size(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 if ("sla".equals(jsonObject.getString("columnAlis"))) {
@@ -37,21 +40,31 @@ public class Test20230427 {
                         String str = "ceil";
                         String substring = content.substring(content.indexOf(str) + str.length() + 1);
                         sla = substring.split("\\*")[0].trim();
+                        weight = substring.split("\\*")[1].split("\\+")[0].trim();
                     }
                 }
             }
 
-            JSONObject ruleSla = JSON.parseObject("{\"columnAlis\":\"ruleSla\",\"extendColumnType\":\"TEMPLATE\"}");
-            ruleSla.put("content", sla);
-            jsonArray.add(ruleSla);
+            JSONObject rule_sla = JSON.parseObject("{\"columnAlis\":\"rule_sla\",\"extendColumnType\":\"TEMPLATE\"}");
+            rule_sla.put("content", sla);
+            jsonArray.add(rule_sla);
+
+            if (StringUtils.isNotBlank(weight)) {
+                JSONObject weightJSON = JSON.parseObject("{\"columnAlis\":\"rule_sla_weight\",\"extendColumnType\":\"TEMPLATE\"}");
+                weightJSON.put("content", weight);
+                jsonArray.add(weightJSON);
+            }
 
             assert sla != null;
             String slqSql = "";
             if (!Integer.valueOf(sla).equals(slaUpdate.getSla())) {
                 slqSql = String.format(", sla=%s", sla);
             }
+
+            String extend_column = JSON.toJSONString(jsonArray);
+            extend_column = extend_column.replace("'", "\\'");
             log.info(String.format("update analysis_model_table set extend_column='%s'%s where id='%s';",
-                    JSON.toJSONString(jsonArray), slqSql, slaUpdate.getId()));
+                    extend_column, slqSql, slaUpdate.getId()));
         }
     }
 
